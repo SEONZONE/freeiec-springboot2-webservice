951e5738f664a323256da6dfbdc51066b85f5fe5
PARK SEONGJONG <48579953+SEONZONE@users.noreply.github.com>
2021-04-02 17:21:58 +0900

    JPA/Hibernate/Spring DATA JPA 학습

--- src/main/java/com/seonzone/book/springboot/Application.java
+++ src/main/java/com/seonzone/book/springboot/Application.java
@@ -1,9 +1,11 @@
 package com.seonzone.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.swing.*;
 
+@EnableJpaAuditing
 @SpringBootApplication
 /*스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정 @SpringBootApplication 의
 위치부터 읽기 때문에 최 상단에 있어야 한다. */
