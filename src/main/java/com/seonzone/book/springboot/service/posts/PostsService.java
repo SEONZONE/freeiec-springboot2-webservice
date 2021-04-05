dcd43b6fcdf6914d8021140cd164712a87e843a4
PARK SEONGJONG <48579953+SEONZONE@users.noreply.github.com>
2021-04-06 01:22:15 +0900

    글 등록/ 전체 조회 기능 추가

--- src/main/java/com/seonzone/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/seonzone/book/springboot/service/posts/PostsService.java
@@ -2,16 +2,21 @@ package com.seonzone.book.springboot.service.posts;
 
 import com.seonzone.book.springboot.domain.posts.Posts;
 import com.seonzone.book.springboot.domain.posts.PostsRepository;
+import com.seonzone.book.springboot.web.dto.PostsListResponseDto;
 import com.seonzone.book.springboot.web.dto.PostsResponseDto;
 import com.seonzone.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seonzone.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
-
+// @Transactional : 선언적 트랜잭션, 클래스, 메서드위에 @Transactional이 추가되면, 이 클래스에 트랜잭션 기능이 적용된 프록시 객체가 생성,
+// 이 프록시 객체는 @Transactional이 포함된 메소드가 호출 될 경우, PlatfromTransactionManager를 사용하여 트랙잭션을 시작하고 정상 여부에 따라
+//Commit 또는 Rollback 한다.
 public class PostsService {
     private final PostsRepository postsRepository;
 
@@ -29,6 +34,21 @@ public class PostsService {
         return id;
 
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+    /* findAllDesc 메소드의 트랜잭션 어노테이션(@Transactional)에 옵션이 하나 추가하였다.
+    * (readOnly = true) 를 주면 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선되기 때문에 등록, 수정, 삭제 기능이 전혀 없는
+    * 서비스 메소드에서 사용하는 것을 추천한다,
+    * .map(PostListResponseDto::new)의 실제로 .map(posts -> new PoststListResponseDto(posts))
+    * postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostListResponseDto 변환 -> List로 반환하는 메소드이다.
+    *
+    * */
+
 
     public PostsResponseDto findById(Long id) {
          Posts entity = postsRepository.findById(id).orElseThrow(() -> new
--- src/main/java/com/seonzone/book/springboot/web/HelloController.java
+++ src/main/java/com/seonzone/book/springboot/web/HelloController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 public class HelloController {
-    @GetMapping("/hello/dto") /* HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
-                             예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었다.
-                             이제 프로젝트의 /hello 로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 된다.*/
-    public HelloResponseDto helloDto(@RequestParam("name") String name,
-                                     @RequestParam("amount") int amount)
-                                    /* 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션이다.
-                                       여기서는 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터
-                                       를 메소드 파라미터 name(String name)에 저장하게 된다.
-                                     */
-    {
-        return new HelloResponseDto(name,amount);
-    }
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+    /* HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어 준다.
+    예전에는 @RequestMapping(method = RequestMethod.GET)으로 사용되었다.
+    이제 프로젝트의 /hello 로 요청이 오면 문자열 hello를 반환하는 기능을 가지게 된다.*/
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount)
+    {
+        return new HelloResponseDto(name,amount);
+    }
 
+      /* 외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션이다.
+      여기서는 외부에서 name(@RequestParam("name"))이란 이름으로 넘긴 파라미터
+       를 메소드 파라미터 name(String name)에 저장하게 된다.
+                             */
 
 
 }
